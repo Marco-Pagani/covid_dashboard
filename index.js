faaf0c76e02a69169313c641d317d9a20bf79960
Marco Pagani <Marco-Pagani@users.noreply.github.com>
2020-04-09 17:29:31 -0400

    tested accessing and displaying data

--- index.js
+++ index.js
@@ -14,14 +14,23 @@ const server = http.createServer((req, res) => {
 
 
 console.log(stats.relative(10));
- /*
+ 
 data.confirmed()
   .then(function (results) {
-    console.log(results.latest);
-    console.log(results.locations[0]);
-    console.log(results.locations[0].history['3/20/20']); 
+    //console.log(results.latest);
+    //console.log(results.locations[0]);
+    //console.log(results.locations[0].history['3/20/20']);
+    var top = results.locations;
+    top = top.sort(function compare(a, b) {
+      return b.latest - a.latest;
     });
-
+    top.length = 5;
+    top.forEach(function (country) {
+      console.log(country.country + ": " + country.latest);
+    });
+    //console.log(top);
+    });
+/*
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
