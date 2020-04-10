7e712724b618387c1835663cce779fecfb869697
Marco Pagani <Marco-Pagani@users.noreply.github.com>
2020-04-10 11:59:02 -0400

    tesking heroku deployment

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
-const http = require('http');
 var data = require('jhucsse.covid');
 var moment = require('moment');
 var stats = require('./stats.js')
 
+const http = require('http');
+const express = require('express')
+const app = express()
+
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end("hello");
+//console.log(stats.relative(10));
+
+
+
+app.get('/', (req, res) => res.send("welcome"));
+
+app.post('/getStats', function (req, res) {
+  data.confirmed()
+    .then(function (results) {
+      //console.log(results.latest);
+      //console.log(results.locations[0]);
+      //console.log(results.locations[0].history['3/20/20']);
+      var top = results.locations;
+      top = top.sort(function compare(a, b) {
+        return b.latest - a.latest;
+      });
+      top.length = 5;
+      top.forEach(function (country) {
+        console.log(country.country + ": " + country.latest);
+      });
+      //console.log(top);
+    });
 });
 
 
-console.log(stats.relative(10));
- 
-data.confirmed()
-  .then(function (results) {
-    //console.log(results.latest);
-    //console.log(results.locations[0]);
-    //console.log(results.locations[0].history['3/20/20']);
-    var top = results.locations;
-    top = top.sort(function compare(a, b) {
-      return b.latest - a.latest;
-    });
-    top.length = 5;
-    top.forEach(function (country) {
-      console.log(country.country + ": " + country.latest);
-    });
-    //console.log(top);
-    });
-/*
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-*/
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
